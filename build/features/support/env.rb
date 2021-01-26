297b49e7959cd0e1ddf43382a816d8baf017c9b6
joão lucas <joao.hilario@picpay.com>
2021-01-26 18:48:58 -0300

    criando estrutura do projeto

---
+++ build/features/support/env.rb
@@ -0,0 +1,9 @@
+require 'appium_lib'
+require 'appium_console'
+
+
+DEVICE = ENV['DEVICE_TYPE']
+
+caps = Appium.load_appium_txt file: File.expand_path("caps/#{DEVICE}/appium.txt", __dir__), verbose: true
+Appium::Driver.new(caps, true)
+Appium.promote_appium_methods Object
